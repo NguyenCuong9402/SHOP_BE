d88541aa02fd93fb3ce688c6dd70363da5a7c771
hung.vu@boot.ai <hung.vu@boot.ai>
2022-07-12 14:33:09 +0700

    BTEST-88 Done

--- app/models.py
+++ app/models.py
@@ -124,32 +124,6 @@ class TestExecutions(db.Model):
     name = db.Column(db.String(255), nullable=True)
 
 
-class MapTestExecutions(db.Model):
-    __tablename__ = 'map_test_exec'
-    id = db.Column(db.String(50), primary_key=True)
-    test_id = db.Column(ForeignKey('tests.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
-    exec_id = db.Column(ForeignKey('test_executions.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
-    index = db.Column(INTEGER())
-    status_id = db.Column(ForeignKey('test_type.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
-    comment = db.Column(db.String(500))
-
-
-class TestRepo(db.Model):
-    __tablename__ = 'test_repo'
-    id = db.Column(db.String(50), primary_key=True)
-    parent_id = db.Column(db.String(50), primary_key=True)
-    name = db.Column(db.String(500))
-    create_date = db.Column(INTEGER(unsigned=True), default=get_timestamp_now(), index=True)
-
-
-class MapRepo(db.Model):
-    __tablename__ = 'map_test_repo'
-    id = db.Column(db.String(50), primary_key=True)
-    test_id = db.Column(ForeignKey('tests.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
-    test_repo_id = db.Column(ForeignKey('test_repo.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
-    create_date = db.Column(INTEGER(unsigned=True), default=get_timestamp_now(), index=True)
-
-
 class Message(db.Model):
     __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -260,3 +234,19 @@ class TestTimer(db.Model):
     date_time = db.Column(db.DATE)
     created_date = db.Column(db.Integer, default=0, index=True)
     modified_date = db.Column(db.Integer, default=0)
+
+
+class TestRepo(db.Model):
+    __tablename__ = 'test_repo'
+    id = db.Column(db.String(50), primary_key=True)
+    parent_id = db.Column(db.String(50), primary_key=True)
+    name = db.Column(db.String(500))
+    create_date = db.Column(INTEGER(unsigned=True), default=get_timestamp_now(), index=True)
+
+
+class MapRepo(db.Model):
+    __tablename__ = 'map_test_repo'
+    id = db.Column(db.String(50), primary_key=True)
+    test_id = db.Column(ForeignKey('tests.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
+    test_repo_id = db.Column(ForeignKey('test_repo.id', ondelete='SET NULL', onupdate='CASCADE'), index=True)
+    create_date = db.Column(INTEGER(unsigned=True), default=get_timestamp_now(), index=True)
