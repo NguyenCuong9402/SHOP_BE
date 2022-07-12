956fad1851fdc1b6a7b67c88f7ab90ad88b9ae99
Thinh Le <thinh.le@boot.ai>
2022-07-12 14:39:51 +0700

    add api get test step fields and test fields

--- app/api/v1/settings.py
+++ app/api/v1/settings.py
@@ -11,15 +11,41 @@ from app.parser import TestFieldSchema, TestStepSchema
 api = Blueprint('settings', __name__)
 
 
-@api.route("/fields/<project_id>", methods=["GET"])
+@api.route("/<project_id>/fields", methods=["GET"])
 def get_test_types_by_project(project_id):
-    test = TestField.query.filter_by().all()
-    test = TestFieldSchema().dump(test)
-    return send_result(data=test, message="OK")
+    # test = TestField.query.filter_by().all()
+    # test = TestFieldSchema().dump(test)
+    data_mock = [
+        {
+            "name": "Issue ID",
+            "key": "btest_field_01"
+        },
+        {
+            "name": "Test Type",
+            "key": "btest_field_02"
+        },
+        {
+            "name": "Test Set Name",
+            "key": "btest_field_03"
+        },
+        {
+            "name": "Test Repository Folder",
+            "key": "btest_field_04"
+        }
+    ]
+    return send_result(data=data_mock, message="OK")
 
 
-@api.route("/test-steps/<project_id>", methods=["GET"])
+@api.route("/<project_id>/test-steps", methods=["GET"])
 def get_test_steps_by_project(project_id):
-    result = TestStep.query.filter_by().all()
-    result = TestStepSchema().dump(result)
-    return send_result(data=result, message="OK")
+    data_mock = [
+        {
+            "name": "Action",
+            "key": "btest_step_field_01"
+        },
+        {
+            "name": "Expected Result",
+            "key": "btest_step_field_02"
+        }
+    ]
+    return send_result(data=data_mock, message="OK")
