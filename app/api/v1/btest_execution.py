698c1da9ac84de98e14f9f9483e39ee3dfa70366
hung.vu@boot.ai <hung.vu@boot.ai>
2022-07-13 14:49:36 +0700

    BTEST-88: create test exec

--- app/api/v1/btest_execution.py
+++ app/api/v1/btest_execution.py
@@ -11,15 +11,54 @@ from marshmallow import ValidationError
 from app.extensions import logger, db
 from app.models import test_test_executions, TestExecutions, MapTestExec, Test
 from app.utils import send_error, send_result
-from app.validator import IssueIDSchema, IssueIDValidator
-from sqlalchemy import func
+from app.validator import IssueIDSchema, IssueIDValidator, TestExecValidator
+from sqlalchemy.sql import func
 
 api = Blueprint('enrollments', __name__)
 
 TEST_EXECUTION_NOT_EXIST = '119'
 ADD_ISSUE_TO_EXECUTION = '18'
 REMOVE_ISSUE_TO_EXECUTION = '16'
+CREATE_TEST_EXECUTION = '19'
+TEST_EXECUTION_EXIST = '20'
 INVALID_PARAMETERS_ERROR = 'g1'
+
+
+@api.route('', methods=['POST'])
+def create_test_exec():
+    """ This api get information of an enrollment_info.
+
+        Returns:
+
+        Examples::
+
+    """
+    # 1. Get keyword from json body
+    try:
+        body = request.get_json()
+        params = TestExecValidator().load(body) if body else dict()
+    except ValidationError as err:
+        logger.error(json.dumps({
+            "message": err.messages,
+            "data": err.valid_data
+        }))
+        return send_error(message_id=INVALID_PARAMETERS_ERROR, data=err.messages)
+
+    exec_id = params.get('id', '')
+    name = params.get('name', '')
+
+    # check test execution exist
+    test_executions: TestExecutions = TestExecutions.query.filter(TestExecutions.id == exec_id).first()
+    if test_executions:
+        return send_error(message_id=TEST_EXECUTION_EXIST)
+
+    new_test_executions = TestExecutions()
+    new_test_executions.id = exec_id
+    new_test_executions.name = name
+    db.session.add(new_test_executions)
+    db.session.commit()
+
+    return send_result(message_id=CREATE_TEST_EXECUTION)
 
 
 @api.route('/<test_execution_id>', methods=['GET'])
@@ -78,9 +117,9 @@ def add_issue_links(test_execution_id):
     if not map_test_executions:
         index = 1
     else:
-        map_test_executions: MapTestExec = MapTestExec.query. \
-            filter(MapTestExec.exec_id == test_execution_id, func.max(MapTestExec.index)).first()
-        index = map_test_executions.index + 1
+        test_executions_index = db.session.query(func.max(MapTestExec.index).label('index')). \
+            filter(MapTestExec.exec_id == test_execution_id).first()
+        index = test_executions_index.index + 1
 
     for item in test_issue:
         new_maps_test_executions = MapTestExec()
