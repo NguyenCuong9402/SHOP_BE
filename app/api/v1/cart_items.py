1343ee33fc5d68a7d4aace530a91d4446bb6f9bc
cuong.nguyen <cuong.nguyen@boot.AI>
2023-07-08 04:00:53 +0700

    ok

--- app/api/v1/cart_items.py
+++ app/api/v1/cart_items.py
@@ -136,8 +136,14 @@ def get_item_to_cart():
     try:
         user_id = get_jwt_identity()
         carts = CartItems.query.filter(CartItems.user_id == user_id).order_by(desc(CartItems.created_date))
-        data = CartItemsSchema(many=True).dump(carts)
-
+        resul = CartItemsSchema(many=True).dump(carts)
+        sum_money = 0
+        for item in resul:
+            sum_money = sum_money + item["price"]
+        data = {
+            "data": resul,
+            "sum_money": sum_money
+        }
         return send_result(data=data)
     except Exception as ex:
         return send_error(message=str(ex))
