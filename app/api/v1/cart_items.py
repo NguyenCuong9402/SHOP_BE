56f9e5792e9b8ea0aedc0fdb1f2823cfbdd4f6ed
cuong.nguyen <cuong.nguyen@boot.AI>
2023-10-24 10:06:37 +0700

    plesea

--- app/api/v1/cart_items.py
+++ app/api/v1/cart_items.py
@@ -112,6 +112,36 @@ def put_item_to_cart(cart_item_id):
         return send_error(message=str(ex))
 
 
+@api.route("/change/<cart_item_id>", methods=["PUT"])
+@jwt_required()
+def put_cart(cart_item_id):
+    try:
+        user_id = get_jwt_identity()
+        body_request = request.get_json()
+        size = body_request.get("size", "")
+        color = body_request.get("color", "")
+        item_cart = CartItems.query.filter(CartItems.id == cart_item_id, CartItems.user_id == user_id).first()
+        if item_cart is None:
+            return send_error(message="Sản phẩm không có trong giỏ hàng, vui lòng F5", is_dynamic=True)
+        if item_cart.size != size and size in ['S', 'M', 'L', 'XL']:
+            item_cart.size = size
+            db.session.flush()
+        if item_cart.color != color and color in item_cart.cac_mau:
+            item_cart.color = color
+            db.session.flush()
+        check = CartItems.query.filter(CartItems.product_id == item_cart.product_id, CartItems.color == item_cart.color,
+                                       CartItems.size == item_cart.size).first()
+        if check is not None:
+            item_cart.quantity = item_cart.quantity + check.quantity
+            CartItems.query.filter(CartItems.id == check.id).delete()
+            db.session.flush()
+        db.session.commit()
+        return send_result(message="Thay đổi số lượng sản phẩm trong giỏ hàng thành công", show=True)
+    except Exception as ex:
+        db.session.rollback()
+        return send_error(message=str(ex))
+
+
 @api.route("", methods=["GET"])
 @jwt_required()
 def get_item_to_cart():
