8d8a6991d87fdeed1a4121ace4b9c0b8f9836fc1
cuong.nguyen <cuong.nguyen@boot.AI>
2023-10-24 13:39:43 +0700

    plesea

--- app/models.py
+++ app/models.py
@@ -167,6 +167,12 @@ class PhanLoai(db.Model):
                           nullable=True)
 
 
+class DiaChiVN(db.Model):
+    __tablename__ = 'dia_chi'
+    id = db.Column(db.String(50), primary_key=True)
+    tinh = db.Column(db.String(50, collation="utf8mb4_vietnamese_ci"), nullable=True)
+    huyen = db.Column(db.String(50, collation="utf8mb4_vietnamese_ci"), nullable=True)
+    xa = db.Column(db.String(50, collation="utf8mb4_vietnamese_ci"), nullable=True)
 
 
 
