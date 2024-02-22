f962812826a8d37cdf62bd358a1e76c66a59b90a
ADMIN <https://nguyen_ngoc_cuong@bitbucket.org/BootAI/btest-backend.git>
2024-02-22 18:21:03 +0700

    Update code

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,6 @@ class Config(object):
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     VERSION = "v1.0.0"
-    FLASK_ADMIN_SWATCH = "cerulean"
 
 
 class DevConfig(Config):
