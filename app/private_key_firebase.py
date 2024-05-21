8f6e2db2098daf30030e1fe02d039b885632ecb0
N.N.Cuong <cuong.nguyen@boot.ai>
2024-05-21 16:39:26 +0700

    update code

---
+++ app/private_key_firebase.py
@@ -0,0 +1,16 @@
+# {
+#   "type": "service_account",
+#   "project_id": "fir-b13c4",
+#   "private_key_id": "57bf1a05e76bee0e87591d6c8ffb3fdb278ad9f9",
+#   "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCez7dBlvq1Nl5l\nrrrUJIOpc2kSjFs+Eqfo0+P74twrZ78MfUa2Tv9kKwYGTRVZrv5GCh//LlqS4qQ8\nW6NmtkUGE6SiSz8vZDx8dCyR9lRdH+iXUSHkm26y5pmAbBQdrdRQpIKKnYYTIi78\n9R4B4v48WSjW1YUbbpmUlziqs+7zs4+CkMZrQS5J9fsmclws3LkranLw3utu8uNW\nTYoRyZBEaNuY3KDqHL8JXQuKE/OIxa89/vH+w3t2nV1dsvtkjN8sjFIM5TuLQIKE\nyC88d6JiE9kVT64/HwXKVcaIcAXHlSFLL1kP9Pn1QVSVVeFa/idTQkmScdKfSkJj\ndZLKbdm3AgMBAAECggEAAyfELjq+sDfXG9+QzSYZPooOOQcxT0jGaqk+VnIqkxXq\nlgBYQGWpjze4qjtWXoVYWdmQpl/BkRTWO4hL0J0q0OpOkaKjPj9PMKIeLgMx/Xv3\nB6JwQHR2/nmWHTt/Wxsgm5P0O78nPor7NZcyOMpf6CbPikViA/aWF65LfelX03eo\nkee2T/Z2xGcmshLqCJKmdcK24+i6A66fFFf3KKdSlhT5rR2Dr3ZUWzf7H8iTwEYf\nZLh+KAS316aerc0vA8mEtIw26WUhF1GDGx8JmNTDzoDJZTjb/3Nwyzf+ixqFgjO/\nPRuWq7qZNVXRJ8jyIvI35tGyvpNGyGYSa1tjAm138QKBgQDRb3kJDnhxA3DLih9u\nHWcYb67PI6MRFHPYWklO4q4hIDqjmHUWPlZEEajnXgSfRUJBkRqtbmtFDRauJmjp\nsRWQ3142wKewJ/iky3l6bkPauqu6ZFiZ/V65saje3SMZtmuIO2zYMjK3Q+bMX3oH\nzgeiVidRfcwUVNiPWJInwK78xwKBgQDCHtompW0Mm332aBJUXKCZCVtRKYU6lcOT\n5GuJOKCYONIyB46Y3R6gcsfjfUX6VoPasIXp1FxqVhysmJVJeybO1XXQT1EwUdGo\nQ4wLpmwNJto2JDQqTeRK/jyPgaLUV4FdUEUg5P+4dO0WTR9LlluzkksFbu0RWCfB\no0siLorrkQKBgB+bxlWilHTiAw0FDJdO4rdia6yhgUPbpVw6zCOWvJcNSSxVnx47\nTEO7spUwsFvlVQmWP8N7Db9/KTQhYeYbLtkNjSeQTf6dGb6tYukiaSVC1RYtfN3I\nypoWARmZoGmKCZKm/VdnxBW6rsObiUxSQP60J/z5kmpjH8gKowxS3VYTAoGBAJ48\nY/tIscGrJlyrrPVTpB3bgLKCM/Xzir+NXRM4M8Ch4MWjX1vAM2z643HO9jIAP+8O\nIbrfvo13sFwI7Pg/CP1lB2pMnZPTviF5HjmI6V83In0rudIaOFPhNiCMvdx1kCAu\nhIsx4DnOJk0dhLsqz4lrYPgG2fxfbdjscRxl8BvhAoGAARLJ0OweEkH4Gm1LPxc8\nDuE2s+Y/U6YrOKUaaDGKYt9YjtTElkA7kGqUqZlKeE7LI38aM+jurYZV8H/24U/i\ngIvtDGrObw9M5ZZewWbeXylcfKHano0PwljQH6q9EdVlK72Lu9554G0CEd216BCd\nlmAQMyYIjEHjsRE7TL8ZgR0=\n-----END PRIVATE KEY-----\n",
+#   "client_email": "firebase-adminsdk-fwm6a@fir-b13c4.iam.gserviceaccount.com",
+#   "client_id": "115503813471860927870",
+#   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#   "token_uri": "https://oauth2.googleapis.com/token",
+#   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fwm6a%40fir-b13c4.iam.gserviceaccount.com",
+#   "universe_domain": "googleapis.com"
+# }
+
+
+# ĐỔi .py thành .json
